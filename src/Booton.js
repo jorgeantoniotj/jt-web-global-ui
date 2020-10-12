2a7f1b63681cfc96f5d37961606442fdb4ef7dde
Jorge <jorgeantoniotj@gmail.com>
2020-10-12 14:35:41 -0500

    Initial commit

---
+++ src/Booton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+    const { children } = props
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Button
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Button from './Booton'
+
+export {
+    Button
+}
